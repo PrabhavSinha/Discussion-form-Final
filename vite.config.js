ecf3e65509e0d26c654f09150b9f43f16c1b9ffd
PrabhavSinha <prabhav1357@gmail>
2026-03-28 08:47:29 +0530

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Discussion-form-Final/',
   plugins: [react()],
   test: {
     globals: true,
